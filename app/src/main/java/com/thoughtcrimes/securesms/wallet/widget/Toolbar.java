e3457b5ddd4b6ea390fbc1a16f064b3cfaf6083f
Hales-Sparrow <hales.sparrow@beldex.in>
2022-11-24 16:44:17 +0530

    Fix:Scan screen showing rescan and setting options in the top.

--- app/src/main/java/com/thoughtcrimes/securesms/wallet/widget/Toolbar.java
+++ app/src/main/java/com/thoughtcrimes/securesms/wallet/widget/Toolbar.java
@@ -89,7 +89,7 @@ public class Toolbar extends MaterialToolbar {
             Timber.d("Set Title if");
             //toolbarImage.setVisibility(View.INVISIBLE);
             toolbarTitle.setVisibility(View.VISIBLE);
-            if(title.equals("Receive")||title.equals("Send")||title.equals("Rescan")){
+            if(title.equals("Receive")||title.equals("Send")||title.equals("Rescan")||title.equals("Scan")){
                 toolBarRescan.setVisibility(View.GONE);
                 toolBarSettings.setVisibility(View.GONE);
             }else{

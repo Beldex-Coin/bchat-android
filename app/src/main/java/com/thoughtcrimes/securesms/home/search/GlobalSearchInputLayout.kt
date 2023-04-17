b80824792d6d4b0c5ed6bf57c899f03a9e45a6f1
SteveJosephh21 <steve.josephh@beldex.io>
2023-04-17 18:22:02 +0530

    Fix : Back pressed issue in Global search screen

--- app/src/main/java/com/thoughtcrimes/securesms/home/search/GlobalSearchInputLayout.kt
+++ app/src/main/java/com/thoughtcrimes/securesms/home/search/GlobalSearchInputLayout.kt
@@ -73,6 +73,10 @@ class GlobalSearchInputLayout @JvmOverloads constructor(
         }
     }
 
+    fun onFocus(){
+        binding.searchInput.requestFocus()
+    }
+
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}

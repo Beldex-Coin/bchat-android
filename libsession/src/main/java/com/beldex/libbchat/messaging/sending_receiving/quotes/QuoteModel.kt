f1b8f85bee631af7fd8f06bf90498344e2c1a2b4
SteveJosephh21 <steve.josephh@beldex.io>
2023-07-20 12:20:57 +0530

    Fix : Visible message alignment issue

--- libsession/src/main/java/com/beldex/libbchat/messaging/sending_receiving/quotes/QuoteModel.kt
+++ libsession/src/main/java/com/beldex/libbchat/messaging/sending_receiving/quotes/QuoteModel.kt
@@ -3,7 +3,7 @@ package com.beldex.libbchat.messaging.sending_receiving.quotes
 import com.beldex.libbchat.messaging.sending_receiving.attachments.Attachment
 import com.beldex.libbchat.utilities.Address
 
-class QuoteModel(val id: Long,
+data class QuoteModel(val id: Long,
                  val author: Address,
                  val text: String?,
                  val missing: Boolean,

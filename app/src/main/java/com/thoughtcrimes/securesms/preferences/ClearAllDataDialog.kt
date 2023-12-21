cb6f9c2611a3db2ce6e3c2b60d99f6319b585739
Hales-Sparrow <hales.sparrow@beldex.in>
2023-12-21 12:16:19 +0530

    Latest UI and functionality implemented for delete entire account feature.

--- app/src/main/java/com/thoughtcrimes/securesms/preferences/ClearAllDataDialog.kt
+++ app/src/main/java/com/thoughtcrimes/securesms/preferences/ClearAllDataDialog.kt
@@ -1,9 +1,11 @@
 package com.thoughtcrimes.securesms.preferences
 
 import android.view.LayoutInflater
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.withContext
@@ -23,24 +25,25 @@ class ClearAllDataDialog : BaseDialog() {
     private lateinit var binding: DialogClearAllDataBinding
 
     enum class Steps {
+        INFO_PROMPT_DEFAULT,
         INFO_PROMPT,
         NETWORK_PROMPT,
         DELETING
     }
 
-    var clearJob: Job? = null
+    private var clearJob: Job? = null
         set(value) {
             field = value
         }
 
-    var step = Steps.INFO_PROMPT
+    private var step = Steps.INFO_PROMPT_DEFAULT
         set(value) {
             field = value
             updateUI()
         }
 
     //Important
-    override fun setContentView(builder: AlertDialog.Builder) {
+/*    override fun setContentView(builder: AlertDialog.Builder) {
         binding = DialogClearAllDataBinding.inflate(LayoutInflater.from(requireContext()))
         binding.cancelButton.setOnClickListener {
             if (step == Steps.NETWORK_PROMPT) {
@@ -55,30 +58,6 @@ class ClearAllDataDialog : BaseDialog() {
                 Steps.NETWORK_PROMPT -> {
                     clearAllData(true)
                 }
-                Steps.DELETING -> { /* do nothing intentionally */ }
-            }
-        }
-        builder.setView(binding.root)
-        builder.setCancelable(false)
-    }
-
-    /*override fun setContentView(builder: AlertDialog.Builder) {
-        binding = DialogClearAllDataBinding.inflate(LayoutInflater.from(requireContext()))
-        binding.cancelButton.setOnClickListener {
-            if (step == Steps.NETWORK_PROMPT) {
-                //clearAllData(false)
-                dismiss()
-            } else if (step != Steps.DELETING) {
-                dismiss()
-            }
-        }
-        binding.clearAllDataButton.setOnClickListener {
-            when(step) {
-                Steps.INFO_PROMPT -> step = Steps.NETWORK_PROMPT
-                Steps.NETWORK_PROMPT -> {
-                    clearAllData(false)
-                    //clearAllData(true)
-                }
                 Steps.DELETING -> { *//* do nothing intentionally *//* }
             }
         }
@@ -86,22 +65,77 @@ class ClearAllDataDialog : BaseDialog() {
         builder.setCancelable(false)
     }*/
 
+    override fun setContentView(builder: AlertDialog.Builder) {
+        binding = DialogClearAllDataBinding.inflate(LayoutInflater.from(requireContext()))
+        val device = RadioOption(
+            "deviceOnly",
+            requireContext().getString(R.string.dialog_clear_all_data_clear_device_only_title),
+            requireContext().getString(R.string.dialog_clear_all_data_clear_device_only_subtitle)
+        )
+        val network = RadioOption(
+            "deviceAndNetwork",
+            requireContext().getString(R.string.dialog_clear_all_data_clear_device_and_network_title),
+            requireContext().getString(R.string.dialog_clear_all_data_clear_device_and_network_subtitle)
+        )
+        var selectedOption = device
+        val optionAdapter = RadioOptionAdapter { selectedOption = it }
+        binding.recyclerView.apply {
+            itemAnimator = null
+            adapter = optionAdapter
+            addItemDecoration(
+                DividerItemDecoration(
+                    requireContext(),
+                    DividerItemDecoration.VERTICAL
+                )
+            )
+            setHasFixedSize(true)
+        }
+        optionAdapter.submitList(listOf(device, network))
+        binding.cancelButton.setOnClickListener {
+            dismiss()
+        }
+        binding.clearAllDataButton.setOnClickListener {
+            when (step) {
+                Steps.INFO_PROMPT_DEFAULT -> step = if (selectedOption == network) {
+                    Steps.NETWORK_PROMPT
+                } else {
+                    Steps.INFO_PROMPT
+                }
+
+                Steps.INFO_PROMPT -> clearAllData(false)
+                Steps.NETWORK_PROMPT -> clearAllData(true)
+                Steps.DELETING -> { /* do nothing intentionally */
+                }
+            }
+        }
+        builder.setView(binding.root)
+        builder.setCancelable(false)
+    }
+
     private fun updateUI() {
         dialog?.let {
             val isLoading = step == Steps.DELETING
 
             when (step) {
                 Steps.INFO_PROMPT -> {
+                    binding.recyclerView.visibility = View.GONE
+                    binding.dialogDescriptionText.visibility = View.VISIBLE
+                    binding.clearAllDataButton.setText(R.string.clear)
                     binding.dialogDescriptionText.setText(R.string.dialog_clear_all_data_explanation)
-                    binding.cancelButton.setText(R.string.cancel)
-                    binding.clearAllDataButton.setText(R.string.delete)
                 }
-                else -> {
+
+                Steps.NETWORK_PROMPT -> {
+                    binding.recyclerView.visibility = View.GONE
+                    binding.dialogDescriptionText.visibility = View.VISIBLE
+                    binding.clearAllDataTitle.setText(R.string.dialog_clear_all_data_clear_device_and_network_title)
                     binding.dialogDescriptionText.setText(R.string.dialog_clear_all_data_network_explanation)
-                    binding.cancelButton.setText(R.string.dialog_clear_all_data_local_only)
-                    binding.clearAllDataButton.setText(R.string.dialog_clear_all_data_clear_network)
-                    binding.cancelButton.setText(R.string.cancel)
-                    binding.clearAllDataButton.setText(R.string.ok)
+                }
+
+                Steps.DELETING -> { /* do nothing intentionally */
+                }
+
+                else -> {
+                    /* do nothing intentionally */
                 }
             }
 

1a97f59b6dfff944d1ac823a9f9f8269d51dce6e
SteveJosephh21 <steve.josephh@beldex.io>
2024-05-14 12:48:14 +0530

    Filter functionality updated for list of fiat currency and address book

--- app/src/main/java/com/thoughtcrimes/securesms/wallet/addressbook/AddressBookScreen.kt
+++ app/src/main/java/com/thoughtcrimes/securesms/wallet/addressbook/AddressBookScreen.kt
@@ -68,250 +68,245 @@ import io.beldex.bchat.R
 fun AddressBookScreen(
 ) {
 
-    val context=LocalContext.current
-    val contactList : MutableList<String> = ArrayList()
+    val context = LocalContext.current
 
     val fromSendScreen by remember {
         mutableStateOf(TextSecurePreferences.getSendAddress(context))
     }
-    var listOfContacts by remember {
-        mutableStateOf(contactList)
+
+    fun listOfContacts(context: Context): List<String> {
+        val contacts = ContactUtilities.getAllContacts(context)
+        return contacts.filter { it.isApproved }.map { it.address.toString() }
     }
+
+    fun getBeldexAddress(publicKey: String): String {
+        val contact = DatabaseComponent.get(context).bchatContactDatabase()
+            .getContactWithBchatID(publicKey)
+        return contact?.displayBeldexAddress(Contact.ContactContext.REGULAR) ?: ""
+    }
+
+    fun getContactList(): List<String> {
+        return listOfContacts(context).filter {
+            getBeldexAddress(it).isNotEmpty()
+        }
+    }
+
+    var listOfContacts by remember {
+        mutableStateOf(getContactList())
+    }
+
     var searchQuery by remember {
         mutableStateOf("")
     }
 
-    fun listOfContacts(context : Context) : List<String> {
-        val contacts=ContactUtilities.getAllContacts(context)
-        return contacts.filter { it.isApproved }.map { it.address.toString() }
-    }
-
-    fun getBeldexAddress(publicKey : String) : String {
-        val contact=DatabaseComponent.get(context).bchatContactDatabase()
-                .getContactWithBchatID(publicKey)
-        return contact?.displayBeldexAddress(Contact.ContactContext.REGULAR) ?: ""
-    }
-
-    fun getUserDisplayName(publicKey : String) : String {
-        val contact=DatabaseComponent.get(context).bchatContactDatabase()
-                .getContactWithBchatID(publicKey)
+    fun getUserDisplayName(publicKey: String): String {
+        val contact = DatabaseComponent.get(context).bchatContactDatabase()
+            .getContactWithBchatID(publicKey)
         return contact?.displayName(Contact.ContactContext.REGULAR) ?: publicKey
     }
 
-    for (d in listOfContacts(context)) {
-        if (getBeldexAddress(d) != "") {
-            contactList.add(d)
-        }
-    }
-
-    fun filterListOfContacts(text : String?, arrayList : MutableList<String>) {
-        val temp : MutableList<String> =ArrayList()
-        for (d in arrayList) {
-            if (getUserDisplayName(d).lowercase().contains(text!!)) {
-                temp.add(d)
+    fun filterContacts() {
+        listOfContacts = getContactList().filter {
+            if (searchQuery.isNotEmpty()) {
+                getUserDisplayName(it).lowercase().contains(searchQuery.lowercase())
+            } else {
+                it.isNotEmpty()
             }
-        }
-        listOfContacts=temp
+        }.toMutableList()
     }
 
-
-    fun copyAddress(beldexAddress : String) {
-        val clipboard=context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val clip=ClipData.newPlainText("Beldex Address", beldexAddress)
+    fun copyAddress(beldexAddress: String) {
+        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("Beldex Address", beldexAddress)
         clipboard.setPrimaryClip(clip)
         Toast.makeText(context, R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show()
     }
 
-    fun redirectToSend(beldexAddress : String, context : Context) {
-        val returnIntent=Intent()
+    fun redirectToSend(beldexAddress: String, context: Context) {
+        val returnIntent = Intent()
         returnIntent.putExtra("address_value", beldexAddress)
         (context as Activity).setResult(RESULT_OK, returnIntent)
         context.finish()
     }
 
     Card(
-            shape=RoundedCornerShape(50),
-            modifier=Modifier
-                    .fillMaxWidth()
-                    .padding(20.dp)
+        shape = RoundedCornerShape(50),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
     ) {
         TextField(
-                value=searchQuery,
-                onValueChange={
-                    searchQuery=it
-                    filterListOfContacts(searchQuery, contactList)
-                },
-                placeholder={
-                    Text(
-                            text=stringResource(id=R.string.search_contact),
-                            style=MaterialTheme.typography.bodyMedium.copy(
-                                    color=MaterialTheme.appColors.inputHintColor
-                            )
+            value = searchQuery,
+            onValueChange = {
+                searchQuery = it
+                filterContacts()
+            },
+            placeholder = {
+                Text(
+                    text = stringResource(id = R.string.search_contact),
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        color = MaterialTheme.appColors.inputHintColor
                     )
-                },
-                colors=TextFieldDefaults.colors(
-                        disabledTextColor=Color.Transparent,
-                        focusedIndicatorColor=Color.Transparent,
-                        unfocusedIndicatorColor=Color.Transparent,
-                        disabledIndicatorColor=Color.Transparent,
-                        focusedContainerColor=MaterialTheme.appColors.searchBackground,
-                        unfocusedContainerColor=MaterialTheme.appColors.searchBackground,
-                        cursorColor=MaterialTheme.appColors.primaryButtonColor,
-                        selectionColors=TextSelectionColors(
-                                handleColor=MaterialTheme.appColors.primaryButtonColor,
-                                backgroundColor=MaterialTheme.appColors.primaryButtonColor
-                        )
-                ),
-                trailingIcon={
+                )
+            },
+            colors = TextFieldDefaults.colors(
+                disabledTextColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                focusedContainerColor = MaterialTheme.appColors.searchBackground,
+                unfocusedContainerColor = MaterialTheme.appColors.searchBackground,
+                cursorColor = MaterialTheme.appColors.primaryButtonColor,
+                selectionColors = TextSelectionColors(
+                    handleColor = MaterialTheme.appColors.primaryButtonColor,
+                    backgroundColor = MaterialTheme.appColors.primaryButtonColor
+                )
+            ),
+            trailingIcon = {
 
-                    IconButton(onClick={
-                        if (searchQuery.isNotEmpty()) {
-                            searchQuery=""
-                            filterListOfContacts("", contactList)
-                        }
-                    }) {
-                        Icon(
-                                imageVector=if (searchQuery.isNotEmpty()) Icons.Default.Clear else Icons.Default.Search,
-                                contentDescription="Search and Clear Icon",
-                                tint=MaterialTheme.appColors.iconTint,
-                        )
+                IconButton(onClick = {
+                    if (searchQuery.isNotEmpty()) {
+                        searchQuery = ""
+                        filterContacts()
                     }
-                },
-                modifier=Modifier
-                        .fillMaxWidth()
+                }) {
+                    Icon(
+                        imageVector = if (searchQuery.isNotEmpty()) Icons.Default.Clear else Icons.Default.Search,
+                        contentDescription = "Search and Clear Icon",
+                        tint = MaterialTheme.appColors.iconTint,
+                    )
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
         )
     }
 
     if (listOfContacts.isEmpty()) {
         Box(
-                contentAlignment=Alignment.Center,
-                modifier=Modifier.fillMaxSize()
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
         ) {
             Text(
-                    text=stringResource(id=R.string.activity_home_empty_state_message),
-                    style=MaterialTheme.typography.titleLarge
+                text = stringResource(id = R.string.activity_home_empty_state_message),
+                style = MaterialTheme.typography.titleLarge
             )
         }
     } else {
-
-
         Column(
-                verticalArrangement=Arrangement.spacedBy(8.dp),
-                modifier=Modifier
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
         ) {
             LazyColumn(
-                    verticalArrangement=Arrangement.spacedBy(16.dp),
-                    modifier=Modifier
-                            .fillMaxWidth()
-                            .weight(1f)
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
             ) {
                 itemsIndexed(listOfContacts) { index, item ->
 
-                    val recipient=Recipient.from(
-                            context,
-                            Address.fromSerialized(item), false
+                    val recipient = Recipient.from(
+                        context,
+                        Address.fromSerialized(item), false
                     )
-                    val threadID=DatabaseComponent.get(context).threadDatabase().getOrCreateThreadIdFor(recipient)
+                    val threadID = DatabaseComponent.get(context).threadDatabase()
+                        .getOrCreateThreadIdFor(recipient)
                     MentionManagerUtilities.populateUserPublicKeyCacheIfNeeded(
-                            threadID,
-                            context
+                        threadID,
+                        context
                     )
-                    val address=recipient.address.serialize()
+                    val address = recipient.address.serialize()
                     Column(
-                            modifier=Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp)) {
-
-
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
                         Row(
-                                verticalAlignment=Alignment.CenterVertically,
-                                horizontalArrangement=Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center,
 
-                                ) {
+                            ) {
                             Card(
-                                    colors=CardDefaults.cardColors(
-                                            containerColor=MaterialTheme.appColors.settingsCardBackground
-                                    ),
-                                    shape=RoundedCornerShape(9.dp),
-                                    elevation=CardDefaults.cardElevation(
-                                            defaultElevation=4.dp
-                                    ),
-                                    modifier=Modifier
-                                            .fillMaxWidth()
-                                            .weight(1f)
-                                            .padding(10.dp)
-                                            .clickable {
-                                            }
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.appColors.settingsCardBackground
+                                ),
+                                shape = RoundedCornerShape(9.dp),
+                                elevation = CardDefaults.cardElevation(
+                                    defaultElevation = 4.dp
+                                ),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                                    .padding(10.dp)
+                                    .clickable {
+                                    }
                             ) {
 
                                 Text(
-                                        text=getUserDisplayName(address),
-                                        style=BChatTypography.titleMedium.copy(
-                                                color=MaterialTheme.appColors.textColor,
-                                                fontSize=14.sp,
-                                                fontWeight=FontWeight(700)
-                                        ),
-                                        modifier=Modifier
-                                                .padding(all=12.dp),
+                                    text = getUserDisplayName(address),
+                                    style = BChatTypography.titleMedium.copy(
+                                        color = MaterialTheme.appColors.textColor,
+                                        fontSize = 14.sp,
+                                        fontWeight = FontWeight(700)
+                                    ),
+                                    modifier = Modifier
+                                        .padding(all = 12.dp),
 
-                                        )
-
-
+                                    )
                             }
                             if (fromSendScreen) {
                                 Box(
-                                        modifier=Modifier
-                                                .width(38.dp)
-                                                .height(40.dp)
-                                                .background(
-                                                        color=MaterialTheme.appColors.primaryButtonColor,
-                                                        shape=RoundedCornerShape(9.dp)
-                                                ),
-                                        contentAlignment=Alignment.Center
+                                    modifier = Modifier
+                                        .width(38.dp)
+                                        .height(40.dp)
+                                        .background(
+                                            color = MaterialTheme.appColors.primaryButtonColor,
+                                            shape = RoundedCornerShape(9.dp)
+                                        ),
+                                    contentAlignment = Alignment.Center
                                 ) {
 
                                     Image(
-                                            painter=painterResource(id=R.drawable.ic_copy_address),
-                                            contentDescription="",
-                                            modifier=Modifier
-                                                    .clickable {
-                                                        copyAddress(getBeldexAddress(address))
-                                                    })
+                                        painter = painterResource(id = R.drawable.ic_copy_address),
+                                        contentDescription = "",
+                                        modifier = Modifier
+                                            .clickable {
+                                                copyAddress(getBeldexAddress(address))
+                                            })
                                 }
-
                             } else {
                                 Box(
-                                        modifier=Modifier
-                                                .width(38.dp)
-                                                .height(40.dp)
-                                                .background(
-                                                        color=MaterialTheme.appColors.cardBackground,
-                                                        shape=RoundedCornerShape(9.dp)
-                                                ),
-                                        contentAlignment=Alignment.Center
+                                    modifier = Modifier
+                                        .width(38.dp)
+                                        .height(40.dp)
+                                        .background(
+                                            color = MaterialTheme.appColors.cardBackground,
+                                            shape = RoundedCornerShape(9.dp)
+                                        ),
+                                    contentAlignment = Alignment.Center
                                 ) {
 
 
                                     Image(
-                                            painter=painterResource(id=R.drawable.share),
-                                            contentDescription="",
-                                            modifier=Modifier
-                                                    .clickable {
-                                                        redirectToSend(getBeldexAddress(address), context)
+                                        painter = painterResource(id = R.drawable.share),
+                                        contentDescription = "",
+                                        modifier = Modifier
+                                            .clickable {
+                                                redirectToSend(getBeldexAddress(address), context)
 
-                                                    })
+                                            })
                                 }
                             }
                         }
                         Text(
-                                text=getBeldexAddress(address),
-                                style=BChatTypography.bodySmall.copy(
-                                        color=MaterialTheme.appColors.secondaryTextColor,
-                                        fontSize=12.sp,
-                                        fontWeight=FontWeight(600)
-                                ),
-                                modifier=Modifier
-                                        .padding(horizontal=15.dp)
+                            text = getBeldexAddress(address),
+                            style = BChatTypography.bodySmall.copy(
+                                color = MaterialTheme.appColors.secondaryTextColor,
+                                fontSize = 12.sp,
+                                fontWeight = FontWeight(600)
+                            ),
+                            modifier = Modifier
+                                .padding(horizontal = 15.dp)
 
                         )
                     }
@@ -331,7 +326,7 @@ fun AddressBookScreenPreview() {
     }
 }
 
-@Preview(uiMode=Configuration.UI_MODE_NIGHT_YES)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun AddressBookScreenPreviewDark() {
     BChatTheme {
--- app/src/main/java/com/thoughtcrimes/securesms/wallet/jetpackcomposeUI/settings/WalletSettingDialogs.kt
+++ app/src/main/java/com/thoughtcrimes/securesms/wallet/jetpackcomposeUI/settings/WalletSettingDialogs.kt
@@ -45,7 +45,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.beldex.libbchat.utilities.TextSecurePreferences
@@ -279,30 +278,28 @@ fun CurrencyDialog(onDismiss : () -> Unit, onClick : (String?) -> Unit) {
     currencyList.add("VEF")
     currencyList.add("ZAR")
 
+    var searchQuery by remember {
+        mutableStateOf("")
+    }
+
     var fiatCurrencyList by remember {
         mutableStateOf(currencyList)
     }
 
-    var searchQuery by remember {
-        mutableStateOf("")
-    }
     val context=LocalContext.current
     var selectedItemIndex by remember {
         mutableStateOf(TextSecurePreferences.getCurrency(context))
     }
     val isDarkTheme=UiModeUtilities.getUserSelectedUiMode(LocalContext.current) == UiMode.NIGHT
 
-    fun filterFiatCurrency(text: String?, arrayList: MutableList<String>) {
-        val temp: MutableList<String> = ArrayList()
-        for (d in arrayList) {
-            //or use .equal(text) with you want equal match
-            //use .toLowerCase() for better matches
-            if (d.lowercase().contains(text!!.lowercase())) {
-                temp.add(d)
+    fun filterFiatCurrency() {
+        fiatCurrencyList = currencyList.filter {
+            if(searchQuery.isNotEmpty()){
+                it.lowercase().contains(searchQuery.lowercase())
+            }else{
+                it.isNotEmpty()
             }
-        }
-        //update recyclerview
-        fiatCurrencyList = temp
+        }.toMutableList()
     }
 
     DialogContainer(
@@ -341,7 +338,7 @@ fun CurrencyDialog(onDismiss : () -> Unit, onClick : (String?) -> Unit) {
                         value =searchQuery,
                         onValueChange ={
                             searchQuery=it
-                            filterFiatCurrency(searchQuery,currencyList)
+                            filterFiatCurrency()
                         },
                         placeHolder =stringResource(R.string.search_currency),
                         shape =RoundedCornerShape(36.dp),
@@ -349,7 +346,7 @@ fun CurrencyDialog(onDismiss : () -> Unit, onClick : (String?) -> Unit) {
                             IconButton(onClick = {
                                 if(searchQuery.isNotEmpty()){
                                     searchQuery = ""
-                                    filterFiatCurrency("",currencyList)
+                                    filterFiatCurrency()
                                 }
                             }) {
                                 Icon(

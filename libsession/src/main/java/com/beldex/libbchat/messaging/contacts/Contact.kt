c3f1f271b864cfa45bf36b62ea1bae1203b27119
SteveJosephh21 <steve.josephh@beldex.io>
2024-06-25 15:57:09 +0530

    isBNSHolder functionality implemented in send and receiver side and isBNSHolder option added in ContactDatabase and Database version upgraded.

--- libsession/src/main/java/com/beldex/libbchat/messaging/contacts/Contact.kt
+++ libsession/src/main/java/com/beldex/libbchat/messaging/contacts/Contact.kt
@@ -39,6 +39,10 @@ class Contact(val bchatID: String) {
      */
     var beldexAddress: String? = null
     /**
+     * The flag is used to verify the BNS holder
+     */
+    var isBnsHolder = false
+    /**
      * The report issue's BChat ID, set by default.
      */
     private val reportIssueBChatID = BuildConfig.REPORT_ISSUE_ID

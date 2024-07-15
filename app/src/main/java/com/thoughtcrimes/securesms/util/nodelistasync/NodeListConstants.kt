2cec32516320d7d8eddb1835b37dc86c0abaa7cf
SteveJosephh21 <steve.josephh@beldex.io>
2024-07-15 16:21:06 +0530

    Dynamic node url is updated

--- app/src/main/java/com/thoughtcrimes/securesms/util/nodelistasync/NodeListConstants.kt
+++ app/src/main/java/com/thoughtcrimes/securesms/util/nodelistasync/NodeListConstants.kt
@@ -1,6 +1,6 @@
 package com.thoughtcrimes.securesms.util.nodelistasync
 
 object NodeListConstants {
-    const val downloadNodeListUrl = "https://testdeb.beldex.dev/Beldex-Projects/Beldex-flutter-wallet/mainnet-apk/testing/01-04-2024/node_list.json"
+    const val downloadNodeListUrl = "https://beldex-flutter-wallet.s3.ap-south-1.amazonaws.com/node_list.json"
     const val downloadNodeListFileName = "node_list.json"
 }

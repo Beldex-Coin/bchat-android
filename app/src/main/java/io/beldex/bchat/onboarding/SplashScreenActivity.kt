d9e1c434aaef5e2da0a9038f4d271d46593a9156
SteveJosephh21 <steve.josephh@beldex.io>
2024-08-30 11:42:20 +0530

    Multiple splash screen issue fixed

--- app/src/main/java/io/beldex/bchat/onboarding/SplashScreenActivity.kt
+++ app/src/main/java/io/beldex/bchat/onboarding/SplashScreenActivity.kt
@@ -3,9 +3,6 @@ package io.beldex.bchat.onboarding
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
 import io.beldex.bchat.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -15,16 +12,13 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Log.d("SplashScreenActivity-->","OK")
         nextPage = intent.extras?.getBoolean("nextPage")!!
-        Handler(Looper.getMainLooper()).postDelayed({
-            if (nextPage) {
-                startActivity(Intent(this, PasswordActivity::class.java))
-                finish()
-            } else {
-                startActivity(Intent(this, LandingActivity::class.java))
-                finish()
-            }
-        }, 1000)
+        if (nextPage) {
+            startActivity(Intent(this, PasswordActivity::class.java))
+            finish()
+        } else {
+            startActivity(Intent(this, LandingActivity::class.java))
+            finish()
+        }
     }
 }

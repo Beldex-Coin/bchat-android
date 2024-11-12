e4b15b2d241d8e591a90af1b63dfc41ff4661e5c
SteveJosephh21 <steve.josephh@beldex.io>
2024-11-12 15:20:13 +0530

    Gradle file is updated

--- libsession/src/main/java/com/beldex/libbchat/messaging/sending_receiving/notifications/Server.kt
+++ libsession/src/main/java/com/beldex/libbchat/messaging/sending_receiving/notifications/Server.kt
@@ -1,6 +1,6 @@
 package com.beldex.libbchat.messaging.sending_receiving.notifications
 
 enum class Server(val url: String, val publicKey: String) {
-    LATEST("http://194.233.68.227:2900", "2c32e3ae8b976b210126d74a47264d883f90d80055a636cbccac2f32d82a5924"),
+    LATEST("http://194.233.68.227:5000", "d6caced35a04f65022468b2854fbe61aa86f415c4832876353dc774a2848b30c"),
     LEGACY("http://notification.rpcnode.stream", "54e8ce6a688f6decd414350408cae373ab6070d91d4512e17454d2470c7cf911")
 }

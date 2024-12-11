c5b70ede12abdf0db0d628544530b9fdd9507696
SteveJosephh21 <steve.josephh@beldex.io>
2024-12-11 09:56:52 +0530

    Push notification server url updated

--- libsession/src/main/java/com/beldex/libbchat/messaging/sending_receiving/notifications/Server.kt
+++ libsession/src/main/java/com/beldex/libbchat/messaging/sending_receiving/notifications/Server.kt
@@ -1,5 +1,5 @@
 package com.beldex.libbchat.messaging.sending_receiving.notifications
 enum class Server(val url: String, val publicKey: String) {
-    LATEST("http://194.233.68.227:5000", "d6caced35a04f65022468b2854fbe61aa86f415c4832876353dc774a2848b30c"),
-    LEGACY("http://3.108.79.216:5000", "589f8d0d376933e6a48266423235f323dfa4eb4179903314cf5dfb30d6cf794a")
+    LATEST("http://pushnotificationv2.rpcnode.stream", "d6caced35a04f65022468b2854fbe61aa86f415c4832876353dc774a2848b30c"),
+    LEGACY("http://pushnotificationv1.rpcnode.stream", "589f8d0d376933e6a48266423235f323dfa4eb4179903314cf5dfb30d6cf794a")
 }

427ad210bcef45c0cd9484eaed858078ba6f312b
SteveJosephh21 <steve.josephh@beldex.io>
2025-01-08 15:43:57 +0530

    Handled ANR issue in set host and host address function

--- app/src/main/java/io/beldex/bchat/home/HomeViewModel.kt
+++ app/src/main/java/io/beldex/bchat/home/HomeViewModel.kt
@@ -2,14 +2,18 @@ package io.beldex.bchat.home
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import io.beldex.bchat.data.NetworkNodes
 import io.beldex.bchat.data.NodeInfo
 import io.beldex.bchat.database.ThreadDatabase
 import io.beldex.bchat.util.Helper
 import io.beldex.bchat.util.SharedPreferenceUtil
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -33,18 +37,26 @@ class HomeViewModel @Inject constructor(
         val selectedNodeId = sharedPreferenceUtil.getSelectedNodeId()
         val storedNodes = sharedPreferenceUtil.getStoredNodes().all
         val nodes = mutableSetOf<NodeInfo>()
-        for (nodeEntry in storedNodes.entries) {
-            val nodeId = nodeEntry.value as String
-            val nodeInfo = NodeInfo.fromString(nodeId)
-            nodeInfo?.let {
-                nodeInfo.isFavourite = true
-                if (nodeId == selectedNodeId) {
-                    nodeInfo.isSelected = true
-                }
-                nodes.add(nodeInfo)
-            } ?: Timber.w("nodeString invalid: %s", nodeId)
+        viewModelScope.launch {
+            val jobsList = ArrayList<Job>()
+            for (nodeEntry in storedNodes.entries) {
+                jobsList.add(
+                    launch {
+                        val nodeId = nodeEntry.value as String
+                        val nodeInfo = NodeInfo.fromString(nodeId)
+                        nodeInfo?.let {
+                            nodeInfo.isFavourite = true
+                            if (nodeId == selectedNodeId) {
+                                nodeInfo.isSelected = true
+                            }
+                            nodes.add(nodeInfo)
+                        } ?: Timber.w("nodeString invalid: %s", nodeId)
+                    }
+                )
+            }
+            jobsList.joinAll()
+            _favouritesNodes.value = nodes.toHashSet()
         }
-        _favouritesNodes.value = nodes.toHashSet()
     }
 
     fun loadLegacyList(legacyListString: String?) {

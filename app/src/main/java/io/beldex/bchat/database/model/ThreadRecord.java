f6a3a098ab67d7197e0c76ef9503e67f1f5afb02
Hales-Sparrow <hales.sparrow@beldex.io>
2025-06-17 11:51:42 +0530

    Added message request response type to ThreadRecord

--- app/src/main/java/io/beldex/bchat/database/model/ThreadRecord.java
+++ app/src/main/java/io/beldex/bchat/database/model/ThreadRecord.java
@@ -142,7 +142,10 @@ public class ThreadRecord extends DisplayRecord implements Serializable {
     } else if (MmsSmsColumns.Types.isScreenshotExtraction(type)) {
       String name = isOutgoing() ? context.getString(R.string.MessageRecord_you) : getRecipient().toShortString();
       return emphasisAdded(context.getString(R.string.ThreadRecord_s_took_a_screenshot, name));
-    } else if (SmsDatabase.Types.isIdentityUpdate(type)) {
+    } else if (MmsSmsColumns.Types.isMessageRequestResponse(type)) {
+      return emphasisAdded(context.getString(R.string.message_requests_accepted));
+      }
+    else if (SmsDatabase.Types.isIdentityUpdate(type)) {
       if (getRecipient().isGroupRecipient()) return emphasisAdded(context.getString(R.string.ThreadRecord_safety_number_changed));
       else                                   return emphasisAdded(context.getString(R.string.ThreadRecord_your_safety_number_with_s_has_changed, getRecipient().toShortString()));
     } else if (SmsDatabase.Types.isIdentityVerified(type)) {

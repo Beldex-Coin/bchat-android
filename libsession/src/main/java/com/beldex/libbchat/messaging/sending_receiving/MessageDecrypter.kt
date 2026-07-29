670032e922a76088b888ae5ea478fdff71fa27c4
SteveJosephh21 <steve.josephh@beldex.io>
2026-07-29 17:16:54 +0530

    Clean up dead code to improve code hygiene

--- libsession/src/main/java/com/beldex/libbchat/messaging/sending_receiving/MessageDecrypter.kt
+++ libsession/src/main/java/com/beldex/libbchat/messaging/sending_receiving/MessageDecrypter.kt
@@ -27,60 +27,6 @@ object MessageDecrypter {
      *
      * @return the padded plaintext.
      */
-    //Main Function
-    /*public fun decrypt(newCiphertext: ByteArray, x25519KeyPair: ECKeyPair): Pair<ByteArray, String> {
-        //New Line
-        val beldexWalletAddress = newCiphertext.sliceArray(0 until 97)
-        val ciphertext = newCiphertext.sliceArray(97 until newCiphertext.size)
-
-        Log.d("@--> beldexWalletaddress ",String(beldexWalletAddress, StandardCharsets.UTF_8))
-        Log.d("@-->cipherText value ", ciphertext.toHexString())
-
-        val recipientX25519PrivateKey = x25519KeyPair.privateKey.serialize()
-        Log.d("D--> recipientX25519PrivateKey ",recipientX25519PrivateKey.toHexString())
-
-        val recipientX25519PublicKey = Hex.fromStringCondensed(x25519KeyPair.hexEncodedPublicKey.removingbdPrefixIfNeeded())
-        Log.d("D--> recipientX25519PublicKey ",recipientX25519PublicKey.toHexString())
-
-        val signatureSize = Sign.BYTES
-        Log.d("D--> signatureSize ",signatureSize.toString())
-
-        val ed25519PublicKeySize = Sign.PUBLICKEYBYTES
-        Log.d("D--> ed25519PublicKeySize ",ed25519PublicKeySize.toString())
-
-        // 1. ) Decrypt the message
-        Log.d("D--> cipherText ",ciphertext.toHexString())
-        val plaintextWithMetadata = ByteArray(ciphertext.size - Box.SEALBYTES)
-        Log.d("D--> plaintextWithMetadata ",plaintextWithMetadata.toHexString())
-
-        try {
-            sodium.cryptoBoxSealOpen(plaintextWithMetadata, ciphertext, ciphertext.size.toLong(), recipientX25519PublicKey, recipientX25519PrivateKey)
-            Log.d("D--> plaintextWithMetadata decrypt",plaintextWithMetadata.toHexString())
-        } catch (exception: Exception) {
-            Log.d("Beldex", "Couldn't decrypt message due to error: $exception.")
-            throw MessageReceiver.Error.DecryptionFailed
-        }
-        if (plaintextWithMetadata.size <= (signatureSize + ed25519PublicKeySize)) { throw MessageReceiver.Error.DecryptionFailed }
-        // 2. ) Get the message parts
-        val signature = plaintextWithMetadata.sliceArray(plaintextWithMetadata.size - signatureSize until plaintextWithMetadata.size)
-        val senderED25519PublicKey = plaintextWithMetadata.sliceArray(plaintextWithMetadata.size - (signatureSize + ed25519PublicKeySize) until plaintextWithMetadata.size - signatureSize)
-        val plaintext = plaintextWithMetadata.sliceArray(0 until plaintextWithMetadata.size - (signatureSize + ed25519PublicKeySize))
-        // 3. ) Verify the signature
-        val verificationData = (plaintext + senderED25519PublicKey + recipientX25519PublicKey)
-        try {
-            val isValid = sodium.cryptoSignVerifyDetached(signature, verificationData, verificationData.size, senderED25519PublicKey)
-            if (!isValid) { throw MessageReceiver.Error.InvalidSignature }
-        } catch (exception: Exception) {
-            Log.d("Beldex", "Couldn't verify message signature due to error: $exception.")
-            throw MessageReceiver.Error.InvalidSignature
-        }
-        // 4. ) Get the sender's X25519 public key
-        val senderX25519PublicKey = ByteArray(Sign.CURVE25519_PUBLICKEYBYTES)
-        sodium.convertPublicKeyEd25519ToCurve25519(senderX25519PublicKey, senderED25519PublicKey)
-
-        return Pair(plaintext, "bd" + senderX25519PublicKey.toHexString())
-    }*/
-
     //Sub Function
     public fun decrypt(ciphertext: ByteArray, x25519KeyPair: ECKeyPair): Triple<ByteArray, String,String> {
 
@@ -128,16 +74,6 @@ object MessageDecrypter {
         // 4. ) Get the sender's X25519 public key
         val senderX25519PublicKey = ByteArray(Sign.CURVE25519_PUBLICKEYBYTES)
         sodium.convertPublicKeyEd25519ToCurve25519(senderX25519PublicKey, senderED25519PublicKey)
-
-        //New Line
-        /* val beldexWalletAddress = plaintext.sliceArray(0 until 97)
-         val newPlainText = plaintext.sliceArray(97 until plaintext.size)
-         val receiveradd = String(beldexWalletAddress, StandardCharsets.UTF_8)
-
-         Log.d("@--> beldexWalletaddress ",String(beldexWalletAddress, StandardCharsets.UTF_8))
-         Log.d("@-->cipherText value ", newPlainText.toHexString())
-
-         return Triple(newPlainText, "bd" + senderX25519PublicKey.toHexString(),receiveradd)*/
 
         var beldexWalletAddress = plaintext.sliceArray(0 until 1)
         var receiveradd = String(beldexWalletAddress, StandardCharsets.UTF_8)
--- libsession/src/main/java/com/beldex/libbchat/messaging/sending_receiving/MessageEncrypter.kt
+++ libsession/src/main/java/com/beldex/libbchat/messaging/sending_receiving/MessageEncrypter.kt
@@ -23,63 +23,6 @@ object MessageEncrypter {
      *
      * @return the encrypted message.
      */
-    //Main Function
-    /*internal fun encrypt(plaintext: ByteArray, recipientHexEncodedX25519PublicKey: String): ByteArray {
-        Log.d("--> plainText ", plaintext.toHexString())
-        val userED25519KeyPair = MessagingModuleConfiguration.shared.getUserED25519KeyPair() ?: throw Error.NoUserED25519KeyPair
-
-        Log.d("--> userED25519KeyPair public key asHexString ",userED25519KeyPair.publicKey.asHexString)
-        Log.d("--> userED25519KeyPair secret key asHexString ",userED25519KeyPair.secretKey.asHexString)
-
-        val recipientX25519PublicKey = Hex.fromStringCondensed(recipientHexEncodedX25519PublicKey.removingbdPrefixIfNeeded())
-
-        Log.d("--> recipientX25519PublicKey",recipientX25519PublicKey.toHexString())
-
-        val verificationData = plaintext + userED25519KeyPair.publicKey.asBytes + recipientX25519PublicKey
-
-        Log.d("--> userED25519KeyPair public key asBytes ",userED25519KeyPair.publicKey.asBytes.toHexString())
-        Log.d("--> userED25519KeyPair secret key asBytes ",userED25519KeyPair.secretKey.asBytes.toHexString())
-        Log.d("--> verificationData ", verificationData.toHexString())
-
-        val signature = ByteArray(Sign.BYTES)
-
-        Log.d("--> signature ", signature.toHexString())
-        try {
-            sodium.cryptoSignDetached(signature, verificationData, verificationData.size.toLong(), userED25519KeyPair.secretKey.asBytes)
-            Log.d("--> signature ", signature.toHexString())
-        } catch (exception: Exception) {
-            Log.d("Beldex", "Couldn't sign message due to error: $exception.")
-            throw Error.SigningFailed
-        }
-        val plaintextWithMetadata = plaintext + userED25519KeyPair.publicKey.asBytes + signature
-
-        Log.d("--> plaintextWithMetadata ", plaintextWithMetadata.toHexString())
-
-        val ciphertext = ByteArray(plaintextWithMetadata.size + Box.SEALBYTES)
-        Log.d("E--> cipherText ",ciphertext.toHexString())
-        try {
-            sodium.cryptoBoxSeal(ciphertext, plaintextWithMetadata, plaintextWithMetadata.size.toLong(), recipientX25519PublicKey)
-            Log.d("EE--> cipherText ",ciphertext.toHexString())
-        } catch (exception: Exception) {
-            Log.d("Beldex", "Couldn't encrypt message due to error: $exception.")
-            throw Error.EncryptionFailed
-        }
-
-        val beldexAddress= "bxdCE36Hq9dZv3biveXck8FHiqbNmsLdyRDhgYPB8zhgRsGRSXnRiq9AmuuCGALMUjeV16PXuir4JHGNgtPKCWK22fzYJN3Ah"
-        val beldexWalletAddress = beldexAddress.toByteArray()
-        Log.d("@--> beldex wallet address bytearray size",beldexWalletAddress.size.toString())
-        Log.d("@--> beldex wallet address",beldexWalletAddress.toHexString())
-        Log.d("@--> beldex wallet address bytearray to string",
-            String(beldexWalletAddress,StandardCharsets.UTF_8)
-        )
-        val cipherTextWithBeldexWalletAddress = beldexWalletAddress+ciphertext
-        Log.d("@--> cipherText with wallet address",cipherTextWithBeldexWalletAddress.toHexString())
-        Log.d("@--> cipherText with wallet address and UTF_8 ",String(ciphertext,StandardCharsets.UTF_8))
-        Log.d("@--> cipherText ",ciphertext.toHexString())
-        Log.d("@--> cipherText ",String(ciphertext,StandardCharsets.UTF_8))
-        return cipherTextWithBeldexWalletAddress
-    }*/
-
     //Sub Function
     internal fun encrypt(plaintext: ByteArray, recipientHexEncodedX25519PublicKey: String,senderBeldexAddress:String): ByteArray {
         //New Line
@@ -135,19 +78,6 @@ object MessageEncrypter {
             throw Error.EncryptionFailed
         }
 
-        /*  val beldexAddress= "bxdCE36Hq9dZv3biveXck8FHiqbNmsLdyRDhgYPB8zhgRsGRSXnRiq9AmuuCGALMUjeV16PXuir4JHGNgtPKCWK22fzYJN3Ah"
-          val beldexWalletAddress = beldexAddress.toByteArray()
-          Log.d("@--> beldex wallet address bytearray size",beldexWalletAddress.size.toString())
-          Log.d("@--> beldex wallet address",beldexWalletAddress.toHexString())
-          Log.d("@--> beldex wallet address bytearray to string",
-              String(beldexWalletAddress,StandardCharsets.UTF_8)
-          )
-          val cipherTextWithBeldexWalletAddress = beldexWalletAddress+ciphertext
-          Log.d("@--> cipherText with wallet address",cipherTextWithBeldexWalletAddress.toHexString())
-          Log.d("@--> cipherText with wallet address and UTF_8 ",String(ciphertext,StandardCharsets.UTF_8))
-          Log.d("@--> cipherText ",ciphertext.toHexString())
-          Log.d("@--> cipherText ",String(ciphertext,StandardCharsets.UTF_8))*/
-        //-Log.d("messageEncryption ciphertext size ", ciphertext.size.toString())
         return ciphertext
     }
 
